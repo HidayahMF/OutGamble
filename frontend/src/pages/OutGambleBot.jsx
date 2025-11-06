64ee9d75804afe0f2ed47a6068f9538b3ee460df
HidayahMF <hidayahmfadillah@gmail.com>
2025-11-06 13:52:55 +0700

    update OutGambleBot

--- frontend/src/pages/OutGambleBot.jsx
+++ frontend/src/pages/OutGambleBot.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import left_arrowbot from "../assets/left_arrowbot.png";
 import logoout from "../assets/logoout.png";
 import { Link } from "react-router-dom";
-// import ikon1 from "../assets/icon1.png";
-// import ikon2 from "../assets/icon2.png";
-// import ikon3 from "../assets/icon3.png";
+import ikon1chatbot from "../assets/ikon1chatbot.png";
+import ikon2chatbot from "../assets/ikon2chatbot.png";
+import ikon3chatbot from "../assets/ikon3chatbot.png";
+import mic from "../assets/mic.png";
+import send from "../assets/send.png";
+import depaninput from "../assets/depaninput.png";
 
 const OutGambleBot = () => {
   return (
@@ -20,11 +23,11 @@ const OutGambleBot = () => {
         <img src={logoout} alt="OutGamble Logo" className="w-24 h-24" />
       </div>
 
-      <div className="text-center mt-6 px-5">
-        <h1 className="text-2xl font-bold">
+      <div className="ml-70 mt-6 px-5">
+        <h1 className="text-[40px] font-semibold">
           Hai, Teman <span className="text-green-500">OutGamble</span>
         </h1>
-        <h2 className="text-xl font-semibold mt-2">
+        <h2 className="text-[35px] font-semibold mt-2">
           Ada yang bisa aku bantu hari ini?
         </h2>
         <p className="text-gray-300 text-sm mt-3">
@@ -34,54 +37,39 @@ const OutGambleBot = () => {
       </div>
 
       <div className="flex flex-col md:flex-row justify-center items-center gap-5 mt-8 px-6">
-        <div className="bg-gradient-to-b from-green-500 to-green-700 rounded-xl p-5 w-full md:w-1/4 flex flex-col items-center text-center">
-          {/* <img src={ikon1} alt="icon" className="w-8 mb-3" /> */}
-          <p className="text-sm">
+        <div className=" bg-linear-to-br from-[#005625] to-[#05B14F] rounded-xl  p-5 md:w-[20%] flex flex-col  ">
+          <img src={ikon1chatbot} alt="icon" className="w-8 mb-3" />
+          <p className="text-semibold">
             Bantu bikin rutinitas biar nggak ketrigger buka situs aneh lagi.
           </p>
         </div>
 
-        <div className="bg-gradient-to-b from-green-500 to-green-700 rounded-xl p-5 w-full md:w-1/4 flex flex-col items-center text-center">
-          {/* <img src={ikon2} alt="icon" className="w-8 mb-3" /> */}
-          <p className="text-sm">
+        <div className=" bg-linear-to-br from-[#005625] to-[#05B14F] rounded-xl p-5 w-full md:w-[20%] flex flex-col  ">
+          <img src={ikon2chatbot} alt="icon" className="w-8 mb-3" />
+          <p className="text-semibold">
             Aturin jadwal harian biar nggak gatel ngeklik deposit.
           </p>
         </div>
 
-        <div className="bg-gradient-to-b from-green-500 to-green-700 rounded-xl p-5 w-full md:w-1/4 flex flex-col items-center text-center">
-          {/* <img src={ikon3} alt="icon" className="w-8 mb-3" /> */}
-          <p className="text-sm">
+        <div className="bg-linear-to-br from-[#005625] to-[#05B14F] rounded-xl p-5 w-full md:w-[20%] flex flex-col  ">
+          <img src={ikon3chatbot} alt="icon" className="w-8 mb-3" />
+          <p className="text-semibold">
             Kasih saran kegiatan seru biar aku lupa sama jackpot palsu.
           </p>
         </div>
       </div>
 
       <div className="mt-auto px-6 py-6 flex justify-center">
-        <div className="bg-white w-full md:w-3/4 rounded-full flex items-center justify-between px-5 py-3">
+        <div className="bg-white w-full md:w-[63%] rounded-2xl flex items-center justify-between px-3 py-2">
+          <img src={depaninput} alt="" className="w-14   cursor-pointer" />
           <input
             type="text"
             placeholder="Tulis pertanyaan anda di sini..."
             className="flex-1 outline-none text-black text-sm placeholder-gray-500"
           />
           <div className="flex items-center gap-3">
-            {/* <img src={micIcon} alt="mic" className="w-5 cursor-pointer" /> */}
-            {/* <img src={sendIcon} alt="send" className="w-5 cursor-pointer" /> */}
-            <button className="bg-green-600 p-2 rounded-full hover:bg-green-700">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-            </button>
+            <img src={mic} alt="mic" className="w-9 cursor-pointer" />
+            <img src={send} alt="send" className="w-12 cursor-pointer" />
           </div>
         </div>
       </div>
