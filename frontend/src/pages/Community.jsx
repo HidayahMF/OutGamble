10472e911f2277f3ff3f21f7f7aa4cc73f482c0d
HidayahMF <hidayahmfadillah@gmail.com>
2025-11-14 22:25:05 +0700

    update komunitas

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import KomunitasOutGamble from "../assets/KomunitasOutGamble.png"
 
 const Community = () => {
   return (
     <section className="bg-black  ">
-      <div>saasdasd</div>
+      <div className="flex flex-row">
+        <div className="flex flex-col items-center justify-center">
+       <img src={KomunitasOutGamble} alt="" className=" w-[30%]"/>
+        <div>
+        <p className="font-regular text-center mt-5 text-[20px]">Temukan komunitas yang mendukung kesadaran digital dan membantu masyarakat menjauhi bahaya judi <br /> online. Bergabunglah untuk belajar, berbagi, dan tumbuh bersama dalam lingkungan positif!</p>
+      </div>
+      </div>
+     
+      </div>
     </section>
   );
 };
