9c494d632622a3dc18310902faaf407598d75039
HidayahMF <hidayahmfadillah@gmail.com>
2025-11-19 23:01:30 +0700

    update komunitas

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import KomunitasOutGamble from "../assets/KomunitasOutGamble.png"
+import KomunitasOutGamble from "../assets/KomunitasOutGamble.png";
+import img1komunitas from "../assets/img1komunitas.png";
 
 const Community = () => {
   return (
     <section className="bg-black  ">
       <div className="flex flex-row">
         <div className="flex flex-col items-center justify-center">
-       <img src={KomunitasOutGamble} alt="" className=" w-[30%]"/>
-        <div>
-        <p className="font-regular text-center mt-5 text-[20px]">Temukan komunitas yang mendukung kesadaran digital dan membantu masyarakat menjauhi bahaya judi <br /> online. Bergabunglah untuk belajar, berbagi, dan tumbuh bersama dalam lingkungan positif!</p>
-      </div>
-      </div>
-     <div className="flex flex-col">
-      <img src="" alt="" />
-      <div>
-        
-      </div>
-     </div>
+          <img src={KomunitasOutGamble} alt="" className=" w-[30%]" />
+          <div>
+            <p className="font-regular text-center mt-5 text-[20px]">
+              Temukan komunitas yang mendukung kesadaran digital dan membantu
+              masyarakat menjauhi bahaya judi <br /> online. Bergabunglah untuk
+              belajar, berbagi, dan tumbuh bersama dalam lingkungan positif!
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <img src={img1komunitas} alt="" />
+          <div></div>
+        </div>
       </div>
     </section>
   );
