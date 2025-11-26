00fce815598f3ec52d75caaded6dd0254c60b33a
HidayahMF <hidayahmfadillah@gmail.com>
2025-11-26 23:51:20 +0700

    update komunitas

--- frontend/src/pages/asiap.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const asiap = () => {
-  return (
-    <div>asiap</div>
-  )
-}
-
-export default asiap
--- frontend/src/pages/asiap2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const asiap2 = () => {
-  return (
-    <div>asiap2</div>
-  )
-}
-
-export default asiap2
